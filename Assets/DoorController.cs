e6acaf2ac0f76190467e5270c34a3e770f62f56c
whoisthatJ <Marat.J.Urmanov@gmail.com>
2023-10-16 10:26:14 +0600

    Project upload

---
+++ Assets/DoorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public float openAngle = 90f;  // угол, на который нужно открыть дверь
+    public float closeAngle = 0f;  // угол, на который нужно закрыть дверь
+    public float smoothTime = 2f;  // время, за которое дверь должна открыться или закрыться
+
+    private Quaternion openRotation;   // кватернион для хранения поворота двери в открытом состоянии
+    private Quaternion closeRotation;  // кватернион для хранения поворота двери в закрытом состоянии
+    private bool isOpening = false;    // флаг, который показывает, открывается ли дверь в данный момент
+
+    void Start()
+    {
+        // Вычисляем кватернионы для поворота двери в открытом и закрытом состоянии
+        openRotation = Quaternion.Euler(0, openAngle, 0);
+        closeRotation = Quaternion.Euler(0, closeAngle, 0);
+    }
+
+    void Update()
+    {
+        // Проверяем, нажата ли кнопка "E"
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Инвертируем значение флага, чтобы открыть или закрыть дверь
+            isOpening = !isOpening;
+        }
+
+        // Вычисляем новый поворот для двери, основываясь на текущем состоянии
+        Quaternion targetRotation = isOpening ? openRotation : closeRotation;
+
+        // Используем метод Slerp для плавного перемещения двери к новому повороту
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothTime * Time.deltaTime);
+    }
+}
---
+++ Assets/Doorqqw.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorqqw : MonoBehaviour
+{
+    public float doorOpenAngle = 90.0f; //The angle the door opens in degrees
+    public float doorCloseAngle = 0.0f; //The angle the door closes in degrees
+    public float smooth = 2.0f; //How smoothly the door opens/closes
+
+    private bool open = false; //Whether or not the door is currently open
+    private Vector3 defaultRotation; //The default rotation of the door
+
+    void Start()
+    {
+        defaultRotation = transform.localEulerAngles; //Set the default rotation to the current local rotation
+        doorOpenAngle += defaultRotation.y; //Adjust the door open angle to the installed angle
+        doorCloseAngle += defaultRotation.y; //Adjust the door close angle to the installed angle
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (open) //If the door is open, close it
+            {
+                Quaternion targetRotation = Quaternion.Euler(defaultRotation.x, doorCloseAngle, defaultRotation.z);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+                open = false;
+            }
+            else //If the door is closed, open it
+            {
+                Quaternion targetRotation = Quaternion.Euler(defaultRotation.x, doorOpenAngle, defaultRotation.z);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+                open = true;
+            }
+        }
+    }
+}
